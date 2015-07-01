f0ca63916a1dfb7af4c8ce6faa76f489154b24f1
smi <smi@gprs-internet-client-10.245.sonicduo.com>
2015-07-01 20:47:12 +0300

    before reinstalling os x

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,6 @@ Rails.application.routes.draw do
       patch :update_userinfo
     end
     resources :posts
-    resources :messages
   end
   resources :sessions, only: [:new, :create, :destroy]
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628101513) do
+ActiveRecord::Schema.define(version: 20150628144225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20150628101513) do
     t.integer  "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "countries", force: :cascade do |t|
